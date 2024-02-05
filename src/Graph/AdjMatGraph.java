aec08809e0992e1afbd8369b9977bdd758cf7a08
ShinSub98 <wjdtjq26@naver.com>
2024-02-05 09:33:10 +0900

    BFS 메소드 로직 수정

--- src/Graph/AdjMatGraph.java
+++ src/Graph/AdjMatGraph.java
@@ -5,9 +5,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 인접행렬 그래프
-// 정점에는 0 혹은 자연수만 삽입할 수 있다.
-// 간선의 유무를 나타내는 2차원 배열인 edge는 0일 경우 간선이 없음을, 1일 경우 간선이 존재함을 의미한다.
+/** <p>인접행렬 그래프</p>
+ * 정점에는 0 혹은 자연수만 삽입할 수 있다.<br>
+ * 간선의 유무를 나타내는 2차원 배열인 edge는 0일 경우 간선이 없음을, 1일 경우 간선이 존재함을 의미한다.
+ */
 public class AdjMatGraph {
     private int size = 0; // 그래프의 현재 정점 개수
     private int max_size; // 그래프의 최대 정점 개수
@@ -26,7 +27,9 @@ public class AdjMatGraph {
     }
 
 
-    // 현재 그래프 내 정점 확인
+    /**
+     * <p>현재 그래프 내 모든 정점 출력</p>
+     */
     public void print_vertices() {
         System.out.printf("%nprint vertices");
         System.out.printf("%nsize = %d%n", size);
@@ -35,7 +38,9 @@ public class AdjMatGraph {
     }
 
 
-    // 현재 그래프 내 간선 확인
+    /**
+     * <p>현재 그래프 내 모든 간선 출력</p>
+     */
     public void print_edges() {
         System.out.printf("%nprint edges%n");
         for (int[] i : edge) {
@@ -45,7 +50,10 @@ public class AdjMatGraph {
     }
 
 
-    // 정점 삽입 메소드
+    /**
+     * 정점 삽입 메소드
+     * @param val 삽입할 key
+     */
     public void insert(int val) {
         // 음수는 삽입 불가
         if (val < 0) {
@@ -76,7 +84,11 @@ public class AdjMatGraph {
     }
 
 
-    // 정점 연결 메소드 (간선 생성)
+    /**
+     * <p>정점을 연결하여 간선을 생성하는 메소드</p>
+     * @param x 연결할 정점1
+     * @param y 연결할 정점2
+     */
     public void link(int x, int y) {
         // 정점이 존재하지 않거나
         // 입력값이 유효하지 않을 때 메소드 종료
@@ -98,7 +110,11 @@ public class AdjMatGraph {
     }
 
 
-    // 간선 삭제 메소드
+    /**
+     * <p>정점 간 연결을 끊어 간선을 삭제하는 메소드</p>
+     * @param x 연결을 끊을 정점1
+     * @param y 연결을 끊을 정점2
+     */
     public void unlink(int x, int y) {
         // 정점이 존재하지 않거나
         // 입력값이 유효하지 않을 때 메소드 종료
@@ -119,7 +135,10 @@ public class AdjMatGraph {
         System.out.printf("Unlinked successfully: %d -/- %d%n", x, y);
     }
 
-    // 정점 삭제 메소드
+    /**
+     * 정점 삭제 메소드
+     * @param val 삭제할 정점의 Key
+     */
     public void delete(int val) {
         // 정점이 존재하지 않을 경우
         if (size == 0) {
@@ -152,8 +171,10 @@ public class AdjMatGraph {
         }
     }
 
-    // 특정 정점과 연결된 다른 정점 찾기
-    // val은 확인할 정점의 값
+    /**
+     * <p>특정 정점과 연결된 다른 정점들을 출력</p>
+     * @param val 기준이 되는 정점
+     */
     public void linked_vertices(int val) {
         if (val < 0) {
             System.out.println("Negative number does not exist in graph");
@@ -183,9 +204,12 @@ public class AdjMatGraph {
     }
 
 
-    // BFS로 두 정점 간 거리 확인
-    // 정점이 존재하지 않을 경우 -1 리턴
-    public void get_distance(int x, int y) {
+    /**
+     * BFS로 두 정점 간 거리 확인 (무방향, 무가중 그래프일 경우 최단거리)
+     * @param x 출발 정점
+     * @param y 도착 정점
+     */
+    public void getDistance(int x, int y) {
         // x와 y가 같을 경우
         if (x == y) {
             System.out.println("same inputs");
@@ -200,20 +224,49 @@ public class AdjMatGraph {
         }
 
         Queue<Integer> queue = new LinkedList<>();
-        boolean[] find = new boolean[max_size];
+        boolean[] visited = new boolean[max_size];
 
         queue.add(index[0]);
-        find[index[0]] = true;
+        visited[index[0]] = true;
+        int distance = 0;
+        int put = 1;
+        int cnt = 0;
+        boolean flag = false;
 
-        int distance = BFS_distance(y, 1, find, queue, 0);
-        System.out.printf("distance =  %d", distance);
+        while (!queue.isEmpty()) {
+            cnt = put;
+            put = 0;
+            for (int i = 0; i < cnt; i++) {
+                int v = queue.poll();
+                if (vertex[v] == y) {
+                    System.out.printf("distance between %d and %d : %d", x, y, distance);
+                    flag = true;
+                    break;
+                }
+                for (int k = 0; k < max_size; k++) {
+                    if (edge[v][k] == 1 && visited[k] == false) {
+                        queue.add(k);
+                        visited[k] = true;
+                        put++;
+                    }
+                }
+            }
+            if (flag) {break;}
+            distance++;
+        }
     }
 
 
-    // index는 출발 정점의 vertex 상에서의 인덱스
-    // target은 목표지점인 정점의 값
-    // distance는 현재까지 탐색한 거리
-    private int BFS_distance(int target, int cnt, boolean[] find, Queue<Integer> queue, int distance) {
+    /**
+     * BFS를 재귀로 구현한 바보짓
+     * @param target 도착 정점
+     * @param cnt 전 level에서 큐에 삽입된 정점의 개수
+     * @param find visited 배열과 같음. 출발 정점은 true로 시작.
+     * @param queue 다음 방문할 정점을 출력하는 큐. 출발 정점을 넣고 시작.
+     * @param distance 현재까지 이동한 거리.
+     * @return 출발 정점부터 도착 정점까지의 거리
+     */
+    private int BFS_recursion(int target, int cnt, boolean[] find, Queue<Integer> queue, int distance) {
         int put = 0;
         for (int i = 0; i < cnt; i++) {
             int index = queue.poll();
@@ -226,13 +279,41 @@ public class AdjMatGraph {
                 }
             }
         }
-        return BFS_distance(target, put, find, queue, distance+1);
+        return BFS_recursion(target, put, find, queue, distance+1);
     }
 
 
-    // 한 정점 존재 유무 확인 메소드
-    // 값이 존재할 경우 인덱스 반환
-    // 값이 존재하지 않을 경우 -1 반환
+    /**
+     * BFS를 통해 그래프의 모든 정점을 출력
+     * @param val BFS 탐색을 시작할 정점의 Key
+     */
+    public void BFS(int val) {
+        int idx = -1;
+        for (int i = 0; i < max_size; i++) {
+            if (vertex[i] == val) {
+                idx = i;
+                break;
+            }
+        }
+
+        // val을 key로 가지는 정점이 존재하지 않을 경우 메소드 종료
+        if (idx == -1) {
+            System.out.println("There is no such vertext");
+            return;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[max_size];
+
+
+    }
+
+
+    /**
+     * 특정 정점이 존재하는지 확인하는 메소드
+     * @param val 존재하는지 확인할 정점의 key
+     * @return 존재할 경우: 정점의 인덱스를 반환<br>존재하지 않을 경우: -1을 반환
+     */
     private int check_vertex(int val) {
         for (int i = 0; i < max_size; i++) {
             // 정점이 존재할 경우 해당 정점의 인덱스 반환
@@ -246,9 +327,12 @@ public class AdjMatGraph {
     }
 
 
-    // 두 정점 존재 유무 확인 메소드
-    // 값이 x, y인 정점이 존재할 경우 {x의 인덱스, y의 인덱스}를 리턴.
-    // 값이 x, y인 정점이 존재하지 않을 경우 {-1, -1}을 리턴.
+    /**
+     * 두 정점이 존재하는지 한 번에 유무를 확인하는 메소드
+     * @param x 확인할 정점 1
+     * @param y 확인할 정점 2
+     * @return 둘 다 존재할 경우: {x의 인덱스, y의 인덱스}를 반환<br>하나라도 존재하지 않을 경우: {-1, -1}을 반환
+     */
     private int[] check_vertices(int x, int y) {
         int[] loc = {-1, -1};
 
